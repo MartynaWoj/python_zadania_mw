bdeb1b2fc5db775c334ffc7e88499dae9247dc9a
Martyna Wojtas <martynawojtas1@gmail.com>
2020-12-04 10:36:49 +0100

    Zadania

--- zad2.5.py
+++ zad2.5.py
@@ -1,4 +1,10 @@
-na_wejsciu = [49, 50, 20, 40, 35, 10]
-na_wyjsciu = [49, 10, 20, 40, 35, 50]
-for indeks, liczby in enumerate(na_wejsciu):
-    print(f'indeks = {10}')
+lista = [49, 50, 20, 40, 35, 10]
+
+minimum = min(lista)
+maksimum = max(lista)
+
+lista[lista.index(minimum)] = maksimum
+lista[lista.index(maksimum)] = minimum
+minimum, maksimum = maksimum, minimum
+
+print(f"{lista}")
